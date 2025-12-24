ded8f82be478732daaf810d340fb5713b4df56a3
Kennyk1 <kennyfavour11@gmail.com>
2025-12-24 20:42:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from flask_cors import CORS  # <-- import CORS
 
 app = Flask(__name__)
+CORS(app)  # <-- enable CORS for all routes
 
 @app.route('/')
 def home():
